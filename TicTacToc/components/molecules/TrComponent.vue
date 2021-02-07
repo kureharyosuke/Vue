d3a781ee95990f1eb58d8a52f93e46dbe19c4ee5
kureharyosuke <ohhayo.com@gmail.com>
2021-02-07 17:47:17 +0900

    TicTacToc components

---
+++ TicTacToc/components/molecules/TrComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <tr>
+    <td-component
+      v-for="(cellData, index) in rowData"
+      :cell-data="cellData"
+      :cell-index="index"
+      :row-index="rowIndex"
+      :key="cellData"
+    ></td-component>
+  </tr>
+</template>
+
+<script>
+import TdComponent from "./components/atoms/TdComponent.vue";
+
+export default {
+  components: {
+    TdComponent,
+  },
+  props: {
+    rowData: Array,
+    rowIndex: Number,
+  },
+};
+</script>
+
+<style></style>
---
+++ TicTacToc/pages/TicTacToc.vue
@@ -0,0 +1,27 @@
+<template>
+    <table-component :table-data="tableData" />
+    <!-- v-bind: 자식컴포넌트 데이터를 건내줄때(TableComponent)-->
+</template>
+
+<script>
+import TableComponent from './components/organlsms/TableComponent.vue'
+export default {
+    components: {
+        TableComponent,
+    },
+    data() {
+        return {
+            tableData:[
+                ['', '', ''],
+                ['', '', ''],
+                ['', '', ''],
+            ],
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style></style>
