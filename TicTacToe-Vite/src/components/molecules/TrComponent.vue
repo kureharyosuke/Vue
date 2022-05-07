59af2837d1559e528071550f8dddee2eaeed21e1
kureharyosuke <ohhayo.com@gmail.com>
2022-05-07 21:45:41 +0900

    vite: tictactoe

---
+++ TicTacToe-Vite/src/components/molecules/TrComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <tr>
+    <td-component
+      v-for="(cellData, index) in rowData"
+      :cell-data="cellData"
+      :cell-index="index"
+      :row-index="rowIndex"
+      :key="index"
+    ></td-component>
+    <!-- 자식(TdComponent)한테 다시 데이터를 넘겨준다. -->
+  </tr>
+</template>
+
+<script>
+import TdComponent from "../atoms/TdComponent.vue";
+
+export default {
+  components: {
+    TdComponent,
+  },
+  props: {
+    // 부모(tableComponent)로부터 데이터를 받고
+    rowData: Array,
+    rowIndex: Number,
+  },
+  data() {
+    return {
+      parent: "Im your father",
+    };
+  },
+};
+</script>
+
+<style></style>
